e2bad40d8516c9ecd049e0cd1d2fccbc3c8db2e3
zxd <saoziasdf1234@sina.com>
2017-03-23 21:51:59 +0800

    popup

--- src/components/util/popUp.js
+++ src/components/util/popUp.js
@@ -1,2 +1,16 @@
-import Vue from 'Vue'
+import Vue from 'vue'
 let hasModal = false;
+
+let instances ={};
+const popManager={
+    zIndex:2000,
+    nextZIndex(){
+      return this.zIndex++;  
+    },
+    getInstance(id){
+        return instances[id];
+    }
+
+}
+
+export default popManager;
